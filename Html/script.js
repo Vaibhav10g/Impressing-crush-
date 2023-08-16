82fdbd29dbeb0a8f3afb742646a7d009f90f5865
Shiv <tacfiregamer@gmail.com>
2023-08-17 03:35:00 +0530

    Fixed the issue #1 with now bg and osm js edits

--- Html/script.js
+++ Html/script.js
@@ -1,84 +1,89 @@
-var radius = 240; // how big of the radius
-var autoRotate = true; // auto rotate or not
-var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
-var imgHeight = 170; // height of images (unit: px)
-
-// Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL = 'https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a';
-var bgMusicControls = true; // Show UI music control
-
-
-
-// ===================== start =======================
-// animation start after 1000 miliseconds
+var radius = 240;
+var autoRotate = true;
+var rotateSpeed = -60;
+var imgWidth = 120;
+var imgHeight = 170;
+var bgMusicURL =
+  "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
+var bgMusicControls = true;
 setTimeout(init, 1000);
-
-var odrag = document.getElementById('drag-container');
-var ospin = document.getElementById('spin-container');
-var aImg = ospin.getElementsByTagName('img');
-var aVid = ospin.getElementsByTagName('video');
-var aEle = [...aImg, ...aVid]; // combine 2 arrays
-
-// Size of images
+var odrag = document.getElementById("drag-container");
+var ospin = document.getElementById("spin-container");
+var aImg = ospin.getElementsByTagName("img");
+var aVid = ospin.getElementsByTagName("video");
+var aEle = [...aImg, ...aVid];
 ospin.style.width = imgWidth + "px";
 ospin.style.height = imgHeight + "px";
-
-// Size of ground - depend on radius
-var ground = document.getElementById('ground');
+var ground = document.getElementById("ground");
 ground.style.width = radius * 3 + "px";
 ground.style.height = radius * 3 + "px";
-
 function init(delayTime) {
   for (var i = 0; i < aEle.length; i++) {
-    aEle[i].style.transform = "rotateY(" + (i * (360 / aEle.length)) + "deg) translateZ(" + radius + "px)";
+    aEle[i].style.transform =
+      "rotateY(" +
+      i * (360 / aEle.length) +
+      "deg) translateZ(" +
+      radius +
+      "px)";
     aEle[i].style.transition = "transform 1s";
     aEle[i].style.transitionDelay = delayTime || (aEle.length - i) / 4 + "s";
   }
 }
-
+function applyRandomShake(obj) {
+  var randomX = Math.random() * 10 - 5;
+  var randomY = Math.random() * 10 - 5;
+  obj.style.transform =
+    "rotateX(" + (-tY + randomY) + "deg) rotateY(" + (tX + randomX) + "deg)";
+}
 function applyTranform(obj) {
-  // Constrain the angle of camera (between 0 and 180)
-  if(tY > 180) tY = 180;
-  if(tY < 0) tY = 0;
-
-  // Apply the angle
-  obj.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+  if (tY > 180) tY = 180;
+  if (tY < 0) tY = 0;
+  applyRandomShake(obj);
 }
-
 function playSpin(yes) {
-  ospin.style.animationPlayState = (yes?'running':'paused');
+  if (yes) {
+    ospin.style.animationPlayState = "running";
+    for (var i = 0; i < aEle.length; i++) {
+      var randomRotation = Math.random() * 10 - 5;
+      aEle[i].style.transition = "transform 1s";
+      aEle[i].style.transform =
+        "rotateY(" +
+        (i * (360 / aEle.length) + randomRotation) +
+        "deg) translateZ(" +
+        radius +
+        "px)";
+    }
+  } else {
+    ospin.style.animationPlayState = "paused";
+  }
 }
-
-var sX, sY, nX, nY, desX = 0,
-    desY = 0,
-    tX = 0,
-    tY = 10;
-
-// auto spin
+var sX,
+  sY,
+  nX,
+  nY,
+  desX = 0,
+  desY = 0,
+  tX = 0,
+  tY = 10;
 if (autoRotate) {
-  var animationName = (rotateSpeed > 0 ? 'spin' : 'spinRevert');
-  ospin.style.animation = `${animationName} ${Math.abs(rotateSpeed)}s infinite linear`;
+  var animationName = rotateSpeed > 0 ? "spin" : "spinRevert";
+  ospin.style.animation = `${animationName} ${Math.abs(
+    rotateSpeed
+  )}s infinite linear`;
 }
-
-// add background music
 if (bgMusicURL) {
-  document.getElementById('music-container').innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls? 'controls': ''} 
-`;
+  document.getElementById("music-container").innerHTML += `
+<audio src="${bgMusicURL}" ${bgMusicControls ? "controls" : ""}></audio>`;
 }
-
-// setup events
 document.onpointerdown = function (e) {
   clearInterval(odrag.timer);
   e = e || window.event;
   var sX = e.clientX,
-      sY = e.clientY;
-
+    sY = e.clientY;
   this.onpointermove = function (e) {
     e = e || window.event;
     var nX = e.clientX,
-        nY = e.clientY;
+      nY = e.clientY;
     desX = nX - sX;
     desY = nY - sY;
     tX += desX * 0.1;
@@ -87,7 +92,6 @@ document.onpointerdown = function (e) {
     sX = nX;
     sY = nY;
   };
-
   this.onpointerup = function (e) {
     odrag.timer = setInterval(function () {
       desX *= 0.95;
@@ -103,11 +107,9 @@ document.onpointerdown = function (e) {
     }, 17);
     this.onpointermove = this.onpointerup = null;
   };
-
   return false;
 };
-
-document.onmousewheel = function(e) {
+document.onmousewheel = function (e) {
   e = e || window.event;
   var d = e.wheelDelta / 20 || -e.detail;
   radius += d;
